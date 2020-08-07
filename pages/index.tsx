1bd12e05b5432a8833958147643868f94714e9a6
Mehul Mohan <mehulmpt@gmail.com>
2020-08-08 00:56:25 +0530

    Next.js template for codedamn runner

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import styles from '../styles/index.module.scss'
+
+const Home: React.FC = () => {
+	return (
+		<div className={styles.hello}>
+			<h1>Hello from codedamn!</h1>
+		</div>
+	)
+}
+
+export default Home
