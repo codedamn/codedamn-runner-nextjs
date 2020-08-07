0a85b6b2d1c99be5dce598d5721faf8360b4e911
Mehul Mohan <mehulmpt@gmail.com>
2020-08-08 01:35:41 +0530

    codedamn runner cannot support TypeScript types right now

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,6 @@
 import styles from '../styles/index.module.scss'
 
-const Home: React.FC = () => {
+const Home = () => {
 	return (
 		<div className={styles.hello}>
 			<h1>Hello from codedamn!</h1>
